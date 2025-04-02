2dc871af98436bf57b5e87cd1b07cdb0329f1e20
Rioto Oka <okarioto@gmail.com>
2025-04-02 12:00:58 -0700

    change royal nails link

--- utils/comission_list.js
+++ utils/comission_list.js
@@ -1,6 +1,6 @@
 const royal_nails = {
     title: "Royal Nails",
-    link: "https://royal-nails.vercel.app",
+    link: "https://royal-nails.kim",
     bullets: ["Developed a fully responsive webpage for a local nail salon business using Next.js, JavaScript, HTML, and CSS, featuring a contact page and optimized for seamless performance across devices.",
         "Designed reusable React components to maintain consistency and improve development efficiency, while ensuring smooth user experience on mobile and desktop views.", "Overcame challenges in optimizing responsiveness through raw CSS and enhanced performance and SEO using Next.js features like server-side rendering (SSR)." ],
     techs: ["JavaScript", "nextJS", "reactJS", "Tailwind", "HTML", "CSS"]
@@ -11,7 +11,7 @@ const badminton_club = {
         "Developed a full-stack web application for a badminton club using React, PostgreSQL, Express, HTML, CSS, and Tailwind, supporting 200 active users with a mobile-first, responsive design.",
         "Built secure CRUD functionality for managing club members and events, including input validation to sanitize database inputs, waitlist support for events exceeding the 50 - user limit, and optimized SQL queries.",
         "Implemented JWT for session management and role - based access control, tracking admin and member roles in the database to enforce permissions effectively.",
-        "Enhanced security and usability with two - step email verification, a 'forgot password' feature(via Nodemailer), secure password hashing(via Argon2), and Axios for seamless API interactions."
+        "Enhanced security and usability with two - step email verification, a 'forgot password' feature (via Nodemailer), secure password hashing(via Argon2), and Axios for seamless API interactions."
 
     ],
     techs: ["JavaScript", "reactJS", "expressJS", "PostgreSQL", "Tailwind", "HTML", "CSS"]
